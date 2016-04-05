6bc007fc031bf5ccb56e831d5021a6cc115daa32
smiley <smiley@chillerlan.net>
2016-04-05 03:35:35 +0200

    cleanup

--- tests/TestGatewayEndpoint.php
+++ tests/TestGatewayEndpoint.php
@@ -11,13 +11,12 @@
 
 namespace chillerlan\ThreemaTest;
 
-use chillerlan\Threema\Crypto\CryptoInterface;
-use chillerlan\Threema\GatewayEndpoint;
-use chillerlan\Threema\GatewayOptions;
-use chillerlan\TinyCurl\Request;
-use chillerlan\TinyCurl\RequestOptions;
-use chillerlan\TinyCurl\Response\ResponseException;
-use chillerlan\TinyCurl\Response\ResponseInterface;
+use chillerlan\Threema\{
+	Crypto\CryptoInterface, GatewayEndpoint, GatewayOptions
+};
+use chillerlan\TinyCurl\{
+	Request, Response\ResponseException, Response\ResponseInterface
+};
 use stdClass;
 
 /**
@@ -30,9 +29,7 @@ class TestGatewayEndpoint extends GatewayEndpoint{
 		$this->cryptoInterface = $cryptoInterface;
 		$this->gatewayOptions  = $gatewayOptions;
 
-		$requestOptions          = new RequestOptions;
-		$requestOptions->ca_info = $this->gatewayOptions->cacert;
-		$this->request = new class($requestOptions) extends Request{
+		$this->request = new class extends Request{
 			protected function getResponse($url){
 				return new class($url) implements ResponseInterface{
 					private $path;
@@ -58,48 +55,26 @@ class TestGatewayEndpoint extends GatewayEndpoint{
 
 					private function getInfo(){
 						$info = new stdClass;
-
-
 						$info->http_code = in_array($this->path[0], ['credits', 'capabilities', 'lookup', 'pubkeys']) ? 200 : 404;
-
 						return $info;
 					}
 
 					private function getBody(){
 						$body = new stdClass;
-
 						switch($this->path[0]){
-							case 'credits':
-								$body->content = getenv('THREEMA_TEST_CREDITS');
-								break;
-							case 'capabilities':
-								$body->content = $this->path[1] === 'ECHOECHO' ? 'image,text,video' : '';
-								break;
+							case 'credits':      $body->content = getenv('THREEMA_TEST_CREDITS'); break;
+							case 'capabilities': $body->content = $this->path[1] === 'ECHOECHO' ? 'image,text,video' : ''; break;
+							case 'pubkeys':      $body->content = $this->path[1] === getenv('THREEMA_TEST_ID') ? getenv('THREEMA_TEST_PUBLIC_KEY') : ''; break;
 							case 'lookup':
-
 								switch($this->path[1]){
-									case 'email_hash':
-										$body->content = $this->path[2] === getenv('THREEMA_TEST_EMAIL_HASH') ? getenv('THREEMA_TEST_ID') : '';
-										break;
-									case 'phone_hash':
-										$body->content = $this->path[2] === getenv('THREEMA_TEST_PHONE_HASH') ? getenv('THREEMA_TEST_ID') : '';
-										break;
-									case 'email':
-										$body->content = $this->path[2] === getenv('THREEMA_TEST_EMAIL') ? getenv('THREEMA_TEST_ID') : '';
-										break;
-									case 'phone':
-										$body->content = $this->path[2] === getenv('THREEMA_TEST_PHONE') ? getenv('THREEMA_TEST_ID') : '';
-										break;
+									case 'email_hash': $body->content = $this->path[2] === getenv('THREEMA_TEST_EMAIL_HASH') ? getenv('THREEMA_TEST_ID') : ''; break;
+									case 'phone_hash': $body->content = $this->path[2] === getenv('THREEMA_TEST_PHONE_HASH') ? getenv('THREEMA_TEST_ID') : ''; break;
+									case 'email':      $body->content = $this->path[2] === getenv('THREEMA_TEST_EMAIL') ? getenv('THREEMA_TEST_ID') : ''; break;
+									case 'phone':      $body->content = $this->path[2] === getenv('THREEMA_TEST_PHONE') ? getenv('THREEMA_TEST_ID') : ''; break;
 								}
-
-								break;
-							case 'pubkeys':
-								$body->content = $this->path[1] === getenv('THREEMA_TEST_ID') ? getenv('THREEMA_TEST_PUBLIC_KEY') : '';
 								break;
 						}
-
 						$body->length = strlen($body->content);
-
 						return $body;
 					}
 
